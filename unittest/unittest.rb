2a0f9e06b9e5d75743e68527b57cc9b992c87e3f
Gary MacDonald <garymd@yahoo-inc.com>
2010-10-06 17:43:57 -0700

    integrate new servicerunner for downloading providers

--- unittest/unittest.rb
+++ unittest/unittest.rb
@@ -25,6 +25,7 @@ class TestLZMA < Test::Unit::TestCase
     end
     @cwd = File.dirname(File.expand_path(__FILE__))
     @service = File.join(@cwd, "../#{subdir}")
+    @providerDir = File.expand_path(File.join(@cwd, "providerDir"))
 # NEEDSWORK!!  DISABLED UNTIL WE FIGURE OUT HOW TO VALIDATE WITHOUT GEMS
 #    @lzma = LZMA.new
   end
@@ -33,12 +34,12 @@ class TestLZMA < Test::Unit::TestCase
   end
 
   def test_load_service
-    BrowserPlus.run(@service) { |s|
+    BrowserPlus.run(@service, @providerDir) { |s|
     }
   end
 
 #  def test_lzma_both_ways
-#    BrowserPlus.run(@service) { |s|
+#    BrowserPlus.run(@service, @providerDir) { |s|
 #      #compressed = @lzma.compress("leroy was here")
 #      #assert_not_nil(compressed)
 #      #assert_equal("leroy was here", @lzma.decompress(compressed))
@@ -46,7 +47,7 @@ class TestLZMA < Test::Unit::TestCase
 #  end
     
 #  def test_decompress_both_encoders
-#    BrowserPlus.run(@service) { |s|
+#    BrowserPlus.run(@service, @providerDir) { |s|
 #      #java_compressed = Base64.decode64("XQAAQAAOAAAAAAAAAAA2GUqrt5owxRYShsh3Etc9r5jV/9OkAAA=")
 #      #assert_equal("leroy was here", @lzma.decompress(java_compressed))
 #      #native_compressed = Base64.decode64("EXQAAgAAOAAAAAAAAAAA2GUqrt5owxRYShsh3EtbQeg4A")
@@ -55,7 +56,7 @@ class TestLZMA < Test::Unit::TestCase
 #  end
 
 #  def test_decompress_sample
-#    BrowserPlus.run(@service) { |s|
+#    BrowserPlus.run(@service, @providerDir) { |s|
 #      #rakefile_compressed = Base64.decode64 %(XQAAgACYAwAAAAAAAAA5GUqKNoFH6SMqlNELWCfVN0/ayvP6n06cnR/N5xv3\noo9rE+JmQnElb8Tya7ikChUSRZBWc5ooDLDJ1se77Hg8CyqQP4MfTg9/vuCZ\n1tAYesF25sS0JKtrKxUF8shctO9Eme6EN6e9G79JIChQ1N2LKXetNnSvtkcS\ngxOP2L2owTeuCRZ1os6lZ/OC2a7mP/1dsxQLWAKU0H66gMtv8x54orKC31IC\nqjRJIe2RSpX31ZorKNxrnoCvt3bVyvF1fJ1XeSpxjJN+OwE9wAOtSRQaeC3b\njf1qODG+J068iIBpYbrEomcriVy5CqiZJdMiumBy2lQsrNHNjfcH4ggB678/\n2b1C2CYxOMOqZQTAz/TcsdjSBzSwSV//yJHP30gZT3H/7o9mn457OgV/l7KG\nAtwYMU/zcAxHWDpTX7V4H4v8YG75aJEfMAYiNcIlV9VU6xy8CvucboFiPIQv\nm7uzw4pnUwr+tKlH335EjovKU5qv+8ZNZaONQUxtnDaB76McQuigAA==\n)
 #      #decompressed = @lzma.decompress(rakefile_compressed)
 #      #assert_match(/require 'rake'/, decompressed)
@@ -66,7 +67,7 @@ class TestLZMA < Test::Unit::TestCase
 #  end
 
 #  def test_compress_testfiletxt
-#    BrowserPlus.run(@service) { |s|
+#    BrowserPlus.run(@service, @providerDir) { |s|
 #      output = s.compress({ 'file' => '/Users/ndmanvar/testfile.txt' })
 #
 #      assert_equal('.', @output[@output.length() - 3].chr)
