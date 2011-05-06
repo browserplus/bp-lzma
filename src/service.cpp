60b309c77721aaf263def2ad6564333e8b73985c
Gary MacDonald <garymd@yahoo-inc.com>
2011-05-05 17:03:32 -0700

    remove bpsdk/bp-service-tools

--- src/service.cpp
+++ src/service.cpp
@@ -30,12 +30,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <ServiceAPI/bperror.h>
-#include <ServiceAPI/bptypes.h>
-#include <ServiceAPI/bpdefinition.h>
-#include <ServiceAPI/bpcfunctions.h>
-#include <ServiceAPI/bppfunctions.h>
-
 #include "bpservice/bpservice.h"
 #include "bputil/bpthread.h"
 #include "bputil/bpsync.h"
