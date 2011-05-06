9527c268c79846a6216157d2d1ccdcb396f4273b
Gary MacDonald <garymd@yahoo-inc.com>
2011-05-06 15:36:02 -0700

    move onto bsf

--- src/service.cpp
+++ src/service.cpp
@@ -31,23 +31,17 @@
  */
 
 #include "bpservice/bpservice.h"
+#include "bpservice/bpcallback.h"
 #include "bputil/bpthread.h"
 #include "bputil/bpsync.h"
 #include "bp-file/bpfile.h"
-
 #include "bpurlutil.cpp"
-
 #include "easylzma/compress.h"  
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <fstream>
-
-#include <iostream>
-#include <list>
-
-const BPCFunctionTable * g_bpCoreFunctions = NULL;
+// When unarchive() is exposed, remove uses of this define.
+// It is currently unexposed because we need to consider
+// all of the security aspects.
+#undef DECOMPRESS_EXPOSED
 
 // Data structures used to manage tasks.  We only allow one task
 // to run per session to put a limit on per-session resource consumption.
@@ -55,138 +49,121 @@ const BPCFunctionTable * g_bpCoreFunctions = NULL;
 // cores, but this is arguably better than allowing any random site to
 // 0wn your proc.
 struct Task {
-    typedef enum { T_Compress, T_Decompress } Type;
-    Task(Type t) : type(t), tid(0), cid(0),
-                   canceled(false) { }
-    ~Task() 
-    {
-        if (inFile.is_open()) {
-            inFile.close();
-        }
-        if (outFile.is_open()) {
-            inFile.close();
+    typedef enum {
+        T_Compress,
+        T_Decompress
+    } Type;
+    Task(Type t, const bplus::service::Transaction& tran, const bplus::CallBack* cb) :
+        m_type(t),
+        m_tran(tran),
+        m_cb(NULL),
+        m_canceled(false) {
+        if (cb != NULL) {
+            m_cb = new bplus::service::Callback(m_tran, *cb);
         }
     }
-    
-    Type type;
-    std::string inPath;
-    std::ifstream inFile;
-    std::string outPath;    
-    std::ofstream outFile;
-    unsigned int tid; 
-    long long cid;
-    bool canceled;
+    ~Task() {
+        if (m_inFile.is_open()) {
+            m_inFile.close();
+        }
+        if (m_outFile.is_open()) {
+            m_outFile.close();
+        }
+        if (m_cb != NULL) {
+            delete m_cb;
+            m_cb = NULL;
+        }
+    }
+    Type m_type;
+    bplus::service::Transaction m_tran;
+    bplus::service::Callback* m_cb;
+    bool m_canceled;
+    std::string m_inPath;
+    std::ifstream m_inFile;
+    std::string m_outPath;    
+    std::ofstream m_outFile;
 };
 
-/* an input callback that will be passed to elzma_compress_run(), 
- * it reads from an opened file */  
 static int  
-inputCallback(void *ctx, void *buf, size_t * size)  
-{
-    Task * t = (Task *) ctx;
-    t->inFile.read((char*)buf, *size);
-    size_t rd = t->inFile.gcount();
-    g_bpCoreFunctions->log(BP_DEBUG, "read %lu bytes from input file",
-                           rd);
+inputCallback(void* ctx, void* buf, size_t* size) {
+    Task* t = (Task*)ctx;
+    t->m_inFile.read((char*)buf, *size);
+    size_t rd = t->m_inFile.gcount();
+    std::stringstream ss;
+    ss << "read " << rd << " bytes from input file";
+    bplus::service::Service::log(BP_DEBUG, ss.str());
     *size = 0;
-    if (rd == 0 && !t->inFile.good()) return -1;
+    if (rd == 0 && !t->m_inFile.good()) {
+        return -1;
+    }
     *size = rd;
     return 0;
 }
   
 static size_t  
-outputCallback(void *ctx, const void *buf, size_t size)  
-{
-    Task * t = (Task *) ctx;
-    g_bpCoreFunctions->log(BP_DEBUG, "Writing %lu bytes to output file",
-                           size);
-  
-    t->outFile.write((char*)buf, size);
-    return t->outFile.bad() ? 0 : size;
+outputCallback(void *ctx, const void *buf, size_t size) {
+    std::stringstream ss;
+    ss << "Writing " << size << " bytes to output file";
+    bplus::service::Service::log(BP_DEBUG, ss.str());
+    Task* t = (Task*)ctx;
+    t->m_outFile.write((char*)buf, size);
+    return t->m_outFile.bad() ? 0 : size;
 }
 
-static
-void progressCallback(void * ctx, size_t complete, size_t total)
-{
-    Task * t = (Task *) ctx;    
-    printf("proggy (%lu) %d/%d\n", t->cid, complete, total);
-    if (t->cid) {
-        double pct = (double) complete / (double) total;
-        g_bpCoreFunctions->invoke(t->tid, t->cid,
-                                  bplus::Double(100.0 * pct).elemPtr());
+static void
+progressCallback(void* ctx, size_t complete, size_t total) {
+    Task* t = (Task*)ctx;    
+    if (t->m_cb) {
+        t->m_cb->invoke(bplus::Double(((double)complete / (double)total) * 100.0));
     }
 }
 
-static
-void performTask(Task * t)
-{
+static void
+performTask(Task* t) {
     assert(t != NULL);
-  
     // open the input file
-    if (!bp::file::openReadableStream(t->inFile, t->inPath, std::ios_base::in | std::ios_base::binary)) {
-        g_bpCoreFunctions->postError(
-            t->tid, "bp.fileAccessError", "Couldn't open file for reading");
+    if (!bp::file::openReadableStream(t->m_inFile, t->m_inPath, std::ios_base::in | std::ios_base::binary)) {
+        t->m_tran.error("bp.fileAccessError", "Couldn't open file for reading");
         return;
     }
-    if (!bp::file::openWritableStream(t->outFile, t->outPath, std::ios_base::out | std::ios_base::binary)) {
-        g_bpCoreFunctions->postError(
-            t->tid, "bp.fileAccessError", "Couldn't open file for writing");
+    if (!bp::file::openWritableStream(t->m_outFile, t->m_outPath, std::ios_base::out | std::ios_base::binary)) {
+        t->m_tran.error("bp.fileAccessError", "Couldn't open file for writing");
         return;
     }
-
-    t->inFile.seekg(0, std::ios::end);
-    size_t sz = t->inFile.tellg();
-    t->inFile.seekg(0, std::ios::beg);
+    t->m_inFile.seekg(0, std::ios::end);
+    size_t sz = t->m_inFile.tellg();
+    t->m_inFile.seekg(0, std::ios::beg);
     if (sz < 0) {
-        g_bpCoreFunctions->postError(
-            t->tid, "bp.fileAccessError", "Couldn't ftell input file");
+        t->m_tran.error("bp.fileAccessError", "Couldn't ftell input file");
         return;
     }
-
     /* use the elzma library to determine a good dictionary size for near
      * optimal compression and minimal runtime memory cost */
     unsigned int dictSz = elzma_get_dict_size(sz);
-
     /* allocate compression handle */  
     elzma_compress_handle hand;
     hand = elzma_compress_alloc();
-      
     /* configure the compression run with mostly default parameters  */   
-    int rc = elzma_compress_config(hand, ELZMA_LC_DEFAULT,  
-                                   ELZMA_LP_DEFAULT, ELZMA_PB_DEFAULT,  
-                                   5, dictSz, ELZMA_lzip, sz);
-      
+    int rc = elzma_compress_config(hand, ELZMA_LC_DEFAULT, ELZMA_LP_DEFAULT, ELZMA_PB_DEFAULT, 5, dictSz, ELZMA_lzip, sz);
     /* fail if we couldn't allocate */    
     if (rc != ELZMA_E_OK) {
         elzma_compress_free(&hand);
-        g_bpCoreFunctions->postError(
-            t->tid, "bp.internalError", "Error allocating compression engine");
+        t->m_tran.error("bp.internalError", "Error allocating compression engine");
         return;
     }
-   
     /* now run the compression */  
     {
         /* run the streaming compression */   
-        rc = elzma_compress_run(hand, inputCallback, (void *) t,  
-                                outputCallback, (void *) t,
-                                progressCallback, (void *) t);
-
+        rc = elzma_compress_run(hand, inputCallback, (void*)t, outputCallback, (void*)t, progressCallback, (void*)t);
         if (rc != ELZMA_E_OK) {
             elzma_compress_free(&hand);
-            g_bpCoreFunctions->postError(
-                t->tid, "bp.compressionError",
-                "Error encountered during compression");
+            t->m_tran.error("bp.compressionError", "Error encountered during compression");
             return;
         }
     }
-
-    t->outFile.close();
-
-    g_bpCoreFunctions->postResults(t->tid, bplus::Path(t->outPath).elemPtr());
-
+    t->m_outFile.close();
+    t->m_tran.complete(bplus::Path(t->m_outPath));
     elzma_compress_free(&hand);
-      
-    return;
 }
 
 // per-session (aka, per-site) data includes a list of tasks to perform,
@@ -202,244 +179,153 @@ struct SessionData {
     std::string tempDir;
 };
 
-static
-void * threadFunc(void * sdp) 
-{
-    SessionData * sd = (SessionData *) sdp;
-
+static void*
+threadFunc(void* pv) {
+    SessionData* sd = (SessionData*)pv;
     sd->mutex.lock();
-
     while (sd->running) {
         if (sd->taskList.size() > 0) {
             sd->mutex.unlock();            
-
             // perform the (de)compression task *outside* of our lock,
             // in the case of async cancelation, the bool 'canceled'
             // will be toggled
             Task* task = *(sd->taskList.begin());
             performTask(task);
-
             sd->mutex.lock();
-
             // the task was either canceled or completed.  in either case
             // we can delete it *inside* the lock
             sd->taskList.erase(sd->taskList.begin());
             delete task;
-
         } else {
             sd->cond.wait(&(sd->mutex));
         }
     }
-
     sd->mutex.unlock();
-
     return NULL;
 }
 
-static int
-BPPAllocate(void ** instance, unsigned int, const BPElement * context)
-{
-    SessionData * sd = new SessionData;
+class LZMA : public bplus::service::Service {
+public:
+BP_SERVICE(LZMA)
+    LZMA();
+    ~LZMA();
+    virtual void finalConstruct();
+    void compress(const bplus::service::Transaction& tran, const bplus::Map& args);
+#if DECOMPRESS_EXPOSED
+    void decompress(const bplus::service::Transaction& tran, const bplus::Map& args);
+#endif // DECOMPRESS_EXPOSED
+private:
+    void lzmaImpl(Task::Type t, const bplus::service::Transaction& tran, const bplus::Map& args);
+private:
+    SessionData* m_sd;
+};
 
-    // extract the temporary directory
-    bplus::Object * args = bplus::Object::build(context);
-    sd->tempDir = (std::string) (*(args->get("temp_dir")));
-    delete args;
+BP_SERVICE_DESC(LZMA, "LZMA", "1.1.0",
+                "Perform LZMA (de)compression.")
+ADD_BP_METHOD(LZMA, compress,
+             "LZMA Compress a file.")
+ADD_BP_METHOD_ARG(compress, "file", Path, true,
+                  "The file that you would like to compress.")
+ADD_BP_METHOD_ARG(compress, "progressCB", CallBack, false,
+                  "A function that will recieve progress callbacks as the compression proceeds.")
+#if DECOMPRESS_EXPOSED
+ADD_BP_METHOD(LZMA, decompress,
+              "Decompress a file compressed with LZMA.")
+ADD_BP_METHOD_ARG(decompress, "file", Path, true,
+                  "The file that you would like to decompress.")
+#endif // DECOMPRESS_EXPOSED
+END_BP_SERVICE_DESC
 
-    boost::filesystem::create_directories(sd->tempDir);
-    g_bpCoreFunctions->log(BP_INFO, "session allocated, using temp dir: %s",
-                           (sd->tempDir.empty() ? "<empty>"
-                                                : sd->tempDir.c_str()));
-
-    sd->running = true;
-    (void) sd->thread.run(threadFunc, (void *) sd);
-    *instance = (void *) sd;
-    return 0;
+LZMA::LZMA() :
+    m_sd(NULL) {
 }
 
-static void
-BPPDestroy(void * instance)
-{
-    assert(instance != NULL);
-    SessionData * sd = (SessionData *) instance;
-
-    // kick the (de)compression thread
-    sd->mutex.lock();
-
-    // mark the first task as canceled to kick the compression thread out
-    // of it's work cycle
-    if (sd->taskList.size() > 0) {
-        (*(sd->taskList.begin()))->canceled = true;
+LZMA::~LZMA() {
+    if (m_sd != NULL) {
+        // kick the (de)compression thread
+        m_sd->mutex.lock();
+        // mark the first task as canceled to kick the compression thread out
+        // of it's work cycle
+        if (m_sd->taskList.size() > 0) {
+            (*(m_sd->taskList.begin()))->m_canceled = true;
+        }
+        m_sd->running = false;
+        m_sd->cond.signal();
+        m_sd->mutex.unlock();
+        // wait for him to shut down
+        m_sd->thread.join();
+        delete m_sd;
+        m_sd = NULL;
     }
-    
-    sd->running = false;
-    sd->cond.signal();
-    sd->mutex.unlock();
-
-    // wait for him to shut down
-    sd->thread.join();
-
-    delete sd;
 }
 
-static void
-BPPShutdown(void)
-{
+void
+LZMA::finalConstruct() {
+    assert(m_sd == NULL);
+    m_sd = new SessionData;
+    m_sd->tempDir = context("temp_dir");
+    boost::filesystem::create_directory(m_sd->tempDir);
+    std::stringstream ss;
+    ss << "session allocated, using temp dir: " << (m_sd->tempDir.empty() ? "<empty>" : m_sd->tempDir.c_str());
+    bplus::service::Service::log(BP_INFO, ss.str());
+    m_sd->running = true;
+    (void)m_sd->thread.run(threadFunc, (void*)m_sd);
+}
+void
+LZMA::compress(const bplus::service::Transaction& tran, const bplus::Map& args) {
+    lzmaImpl(Task::T_Compress, tran, args);
 }
 
-static void
-BPPInvoke(void * instance, const char * funcName,
-          unsigned int tid, const BPElement * elem)
-{
-    assert(instance != NULL);
-    SessionData * sd = (SessionData *) instance;
+#if DECOMPRESS_EXPOSED
+void
+LZMA::decompress(const bplus::service::Transaction& tran, const bplus::Map& args) {
+    lzmaImpl(Task::T_Decompress, tran, args);
+}
+#endif // DECOMPRESS_EXPOSED
 
-    Task::Type t;
-    
-    if (!strcmp(funcName, "compress")) {
-        t = Task::T_Compress;
-    } else if (!strcmp(funcName, "decompress")) {        
-        t = Task::T_Decompress;
-    } else {
-        g_bpCoreFunctions->log(BP_ERROR, "invalid function invoked!");
-    }
-    
-
-    bplus::Object * args = NULL;
-    if (elem) args = bplus::Object::build(elem);
-
-    // all functions have a "file" argument, validate we can parse it
-    std::string url = (*(args->get("file")));
-    std::string path = bp::urlutil::pathFromURL(url);
-
-    if (path.empty())
-    {
-        g_bpCoreFunctions->log(
-            BP_ERROR, "can't parse file:// url: %s", url.c_str());
-
-        g_bpCoreFunctions->postError(
-            tid, "bp.fileAccessError", "invalid file URI");
-
-        if (args) delete args;
+void
+LZMA::lzmaImpl(Task::Type t, const bplus::service::Transaction& tran, const bplus::Map& args) {
+    std::stringstream ss;
+    assert(m_sd != NULL);
+    // first we'll get the input file into a string
+    const bplus::Path* bpPath = dynamic_cast<const bplus::Path*>(args.value("file"));
+// NEEDSWORK!!!  Fix this when port to v5 since we don't need URI's anymore
+#if 0
+    std::string path = bpPath;
+#else // 0
+    std::string path = bp::urlutil::pathFromURL((std::string)(*bpPath));
+#endif // 0
+    if (path.empty()) {
+        ss.str("");
+        ss << "can't parse file:// url: " << (std::string)(*bpPath);
+        log(BP_ERROR, ss.str());
+        tran.error("bp.fileAccessError", "invalid file URI");
         return;
-    } 
-
+    }
     // generate the output path
-    boost::filesystem::path  outPath = bp::file::getTempPath(sd->tempDir, path);
+    boost::filesystem::path outPath = bp::file::getTempPath(m_sd->tempDir, path);
     // append "lz"
     outPath.replace_extension(".lz");
-    
-    if (outPath.string().empty())
-    {
-        g_bpCoreFunctions->postError(tid, "bp.internalError",
-                                     "can't generate output path");
-        if (args) delete args;
+    if (outPath.string().empty()) {
+        tran.error("bp.internalError", "can't generate output path");
         return;
     } 
-
-    // Allocate the Task structure
-    Task* task = new Task(t);
-    task->tid = tid;
-    task->inPath = path;
-    task->outPath = outPath.string();
-    
     // append callback if available
-    if (args->has("progressCB", BPTCallBack)) {
-        task->cid = (long long) *(args->get("progressCB"));
+    const bplus::CallBack* cb = NULL;
+    if (args.has("progressCB")) {
+        cb = dynamic_cast<const bplus::CallBack*>(args.value("progressCB"));
     }
-
-    g_bpCoreFunctions->log(BP_INFO, "LZMA compressing '%s' to '%s'",
-                           task->inPath.c_str(),
-                           task->outPath.c_str());
-    
+    // Allocate the Task structure
+    Task* task = new Task(t, tran, cb);
+    task->m_inPath = path;
+    task->m_outPath = outPath.string();
+    ss.str("");
+    ss << "LZMA compressing '" << task->m_inPath << "' to '" << task->m_outPath << "'";
+    bplus::service::Service::log(BP_INFO, ss.str());
     // add task to work queue and wakeup compression thread
-    sd->mutex.lock();
-    sd->taskList.push_back(task);
-    sd->cond.signal();
-    sd->mutex.unlock();
-    
-    if (args) delete args;
-
-    return;
+    m_sd->mutex.lock();
+    m_sd->taskList.push_back(task);
+    m_sd->cond.signal();
+    m_sd->mutex.unlock();
 }
 
-
-BPArgumentDefinition s_compressFuncArgs[] = {
-    {
-        "file",
-        "The file that you would like to compress.",
-        BPTPath,
-        BP_TRUE
-    },
-    {
-        "progressCB",
-        "A function that will recieve progress callbacks as the compression proceeds.",
-        BPTCallBack,
-        BP_FALSE
-    }
-};
-
-BPArgumentDefinition s_decompressFuncArgs[] = {
-    {
-        "file",
-        "The file that you would like to decompress.",
-        BPTPath,
-        BP_TRUE
-    }
-};
-
-
-static BPFunctionDefinition s_functions[] = {
-    {
-        "compress",
-        "LZMA Compress a file.",
-        sizeof(s_compressFuncArgs)/sizeof(s_compressFuncArgs[0]),
-        s_compressFuncArgs
-    }
-#if 0
-    // version 1.0.x will not include support for decompression,
-    // we need to add some other features in the BrowserPlus platform
-    // to allow the user to securely select a writable location
-    ,{
-        "decompress",
-        "Decompress a file compressed with LZMA.",
-        sizeof(s_decompressFuncArgs)/sizeof(s_decompressFuncArgs[0]),
-        s_decompressFuncArgs
-    }
-#endif
-};
-
-const BPCoreletDefinition *
-BPPInitialize(const BPCFunctionTable * bpCoreFunctions,
-              const BPElement * parameterMap)
-{
-    // a description of this service
-    static BPCoreletDefinition s_serviceDef = {
-        "LZMA",
-        1, 0, 3,
-        "Perform LZMA (de)compression.",
-        sizeof(s_functions)/sizeof(s_functions[0]),
-        s_functions
-    };
-
-    g_bpCoreFunctions = bpCoreFunctions;
-    return &s_serviceDef;
-}
-
-/** and finally, declare the entry point to the corelet */
-BPPFunctionTable funcTable = {
-    BPP_CORELET_API_VERSION,
-    BPPInitialize,
-    BPPShutdown,
-    BPPAllocate,
-    BPPDestroy,
-    BPPInvoke,
-    NULL,
-    NULL
-};
-
-const BPPFunctionTable *
-BPPGetEntryPoints(void)
-{
-    return &funcTable;
-}
